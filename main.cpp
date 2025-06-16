8cc837cd42251353af2050061d0835dba3fc5298
KoudaAyu <k024g0061@g.neec.ac.jp>
2025-06-16 15:23:42 +0900

    windowの生成。x押したら閉じる

--- main.cpp
+++ main.cpp
@@ -1,8 +1,88 @@
 #include<Windows.h>
 
+#include<cstdint>
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//アプリケーションを終了する
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	WNDCLASS wc{};
+
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+
+	//ウィンドウクラス名
+	wc.lpszClassName = L"MyWindowClass";
+
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+	//クライアント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
+
+	//クライアント領域をもとに実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName, //ウィンドウクラス名
+		L"DirectX Window", //ウィンドウタイトル
+		WS_OVERLAPPEDWINDOW, //ウィンドウスタイル
+		CW_USEDEFAULT, CW_USEDEFAULT, //位置
+		wrc.right - wrc.left, wrc.bottom - wrc.top, //サイズ
+		nullptr, //親ウィンドウハンドル
+		nullptr, //メニューハンドル
+		wc.hInstance, //インスタンスハンドル
+		nullptr //追加のパラメータ
+	);
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+	//ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT)
+	{
+		//Windowに目セージが来ていたら最優先で処理される
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg); //メッセージを変換
+			DispatchMessage(&msg); //メッセージをウィンドウプロシージャに送る
+		}
+		else
+		{
+			//ゲームの処理
+		}
+	}
+
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
 	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello, DirextX!\n");
 
