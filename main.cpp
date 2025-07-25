6f73c830944b226ecdebecda984695aaec9db523
KoudaAyu <k024g0061@g.neec.ac.jp>
2025-07-25 09:57:39 +0900

    Sound の class化

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include"DebugCamera.h"
 #include"KeyInput.h"
 #include"Matrix4x4.h"
+#include"Sound.h"
 #include"Vector.h"
 #include"GameScene.h"
 
@@ -115,34 +116,6 @@ struct D3DResourceLeakChecker
 	}
 };
 
-//チャンクヘッダ
-struct ChunkHeader
-{
-	char id[4]; // チャンクID
-	uint32_t size; // チャンクのサイズ
-};
-//RIFFヘッダチャンク
-struct RiffHeader
-{
-	ChunkHeader chunk; // チャンクヘッダ
-	char type[4]; // フォーマット（"WAVE"）
-};
-
-struct FormatChunk
-{
-	ChunkHeader chunk; //fmt
-	WAVEFORMATEX fmt; // フォーマット情報
-};
-
-struct SoundData
-{
-	//波型フォーマット
-	WAVEFORMATEX wfex;
-	//バッファの先頭アドレス
-	BYTE* pBuffer;
-	//バッファのサイズ
-	unsigned int bufferSize;
-};
 
 
 std::wstring ConvertString(const std::string& str)
@@ -595,100 +568,6 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 	return modelData;
 }
 
-SoundData SoundLoadWave(const char* filename)
-{
-	/*HRESULT result;*/
-
-	//ファイルを開く
-	std::ifstream file;
-	//.wavファイルをバイナリで開く
-	file.open(filename, std::ios_base::binary);
-	//ファイルが開けなかった
-	assert(file.is_open());
-
-	//wavデータの読み込み
-	//RIFFヘッダーの読み込み
-	RiffHeader riff;
-	file.read((char*)&riff, sizeof(riff));
-	//ファイルがRIFFかチェック
-	if (strncmp(riff.chunk.id, "RIFF", 4) != 0)
-	{
-		assert(0);
-	}
-	//タイプがWAVEかチェック
-	if (strncmp(riff.type, "WAVE", 4) != 0)
-	{
-		assert(0);
-	}
-	//Formatチャンクの読み込み
-	FormatChunk format = {};
-	//チャンクヘッダーの読み込み
-	file.read((char*)&format, sizeof(ChunkHeader));
-	if (strncmp(format.chunk.id, "fmt ", 4) != 0) // "fmt" の後にスペースを追加
-	{
-		assert(0);
-	}
-	//チャンク本体の読み込み
-	assert(format.chunk.size <= sizeof(format.fmt));
-	file.read((char*)&format.fmt, format.chunk.size);
-	//Dataチャンクの読み込み
-	ChunkHeader data;
-	file.read((char*)&data, sizeof(data));
-	//JUNKチャンクを検出した場合
-	if (strncmp(data.id, "JUNK", 4) == 0)
-	{
-		//読み取り位置をJUNKチャンクの終わりまで進める
-		file.seekg(data.size, std::ios_base::cur);
-		//再度読み込み
-		file.read((char*)&data, sizeof(data));
-	}
-	if (strncmp(data.id, "data", 4) != 0)
-	{
-		assert(0);
-	}
-	//Dataチャンクのデータ部(波形データ)の読み込み
-	char* pBuffer = new char[data.size];
-	file.read(pBuffer, data.size);
-	//Waveファイルを閉じる
-	file.close();
-
-	//returnする為の音声データ
-	SoundData soundData = {};
-	soundData.wfex = format.fmt; //Waveフォーマット情報をセット
-	soundData.pBuffer = reinterpret_cast<BYTE*>(pBuffer);
-	soundData.bufferSize = data.size;
-
-	return soundData;
-}
-
-void SoundUnload(SoundData* soundData)
-{
-	//バッファのメモリを解放
-	delete soundData->pBuffer;
-	soundData->pBuffer = 0;
-	soundData->bufferSize = 0;
-	soundData->wfex = {};
-}
-
-void SoundPlayWave(Microsoft::WRL::ComPtr<IXAudio2>& xAudio2, const SoundData& soundData)
-{
-	HRESULT result;
-
-	//波形フォーマットを元にSourceVoiceの生成
-	IXAudio2SourceVoice* pSourceVoice = nullptr;
-	result = xAudio2->CreateSourceVoice(&pSourceVoice, &soundData.wfex);
-	assert(SUCCEEDED(result)); //SourceVoiceの生成に失敗したら停止
-
-	//再生する波形データの設定
-	XAUDIO2_BUFFER buf{};
-	buf.pAudioData = soundData.pBuffer;
-	buf.AudioBytes = soundData.bufferSize;
-	buf.Flags = XAUDIO2_END_OF_STREAM;
-
-	//波形データの再生
-	result = pSourceVoice->SubmitSourceBuffer(&buf);
-	result = pSourceVoice->Start();
-}
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
@@ -1497,27 +1376,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	bool drawSphere = true;
 	bool drawSprite = false;
 
-	//サウンド関係
-	Microsoft::WRL::ComPtr<IXAudio2> xAudio2;
-	IXAudio2MasteringVoice* masterVoice = nullptr;
-
-	// XAudio2の初期化
-	HRESULT result = XAudio2Create(&xAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR);
-	if (FAILED(result))
-	{
-		assert(0 && "XAudio2Create failed");
-	}
-
-	result = xAudio2->CreateMasteringVoice(&masterVoice);
-	if (FAILED(result))
-	{
-		assert(0 && "CreateMasteringVoice failed");
-	}
-	//音声読み込み
-	SoundData soundData = SoundLoadWave("Resources/Alarm01.wav");
-	//音声再生
-	SoundPlayWave(xAudio2, soundData);
-
 	KeyInput inputManager;
 	inputManager.Initialize(hInstance, hwnd);
 
@@ -1527,7 +1385,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 	GameScene gameScene;
 	gameScene.Initialize();
 
-
+	Sound sound;
+	sound.Initialize();
+	sound.Load();
+	sound.Play();
 
 
 	//Imguiの初期化
@@ -1751,8 +1612,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 
 	CloseHandle(fenceEvent);
 
-	xAudio2.Reset();
-	SoundUnload(&soundData);
+	/*xAudio2.Reset();
+	SoundUnload(&soundData);*/
+
+	sound.~Sound();
 
 
 	delete[] vertexData;
