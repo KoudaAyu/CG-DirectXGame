e6c2832122036cef55a74de00c812d068d592055
AyuKouda <k024g0061@g.neec.ac.jp>
2026-07-01 16:20:14 +0900

    Expose Emitter via getter in GamePlayScene and add spawn count/frequency sliders in DebugUI

--- project/Baziru3_Engine/Base/DebugUI.cpp
+++ project/Baziru3_Engine/Base/DebugUI.cpp
@@ -5,6 +5,7 @@
 #include "SceneManager.h"
 #include "OffScreenRendering.h"
 #include "ParticleManager.h"
+#include "Application/Scene/GameScene/GamePlayScene.h"
 #include <imgui.h>
 
 DebugUI::DebugUI(MaterialManager* materialManager, SpriteManager* spriteManager, Camera* camera,
@@ -231,6 +232,23 @@ void DebugUI::Update()
             uint32_t activeCount = pm->GetNumInstance();
             ImGui::Text("Active Count: %u particles", activeCount);
             ImGui::ProgressBar(static_cast<float>(activeCount) / 10240.0f, ImVec2(0.0f, 0.0f), "Spawn Load");
+
+            BaseScene* currentScene = SceneManager::GetInstance()->GetCurrentScene();
+            GamePlayScene* gps = dynamic_cast<GamePlayScene*>(currentScene);
+            if (gps)
+            {
+                ImGui::Separator();
+                ImGui::Text("Emitter Settings:");
+                Emitter& emitter = gps->GetEmitter();
+
+                int countVal = static_cast<int>(emitter.count);
+                if (ImGui::SliderInt("Spawn Count", &countVal, 1, 1000))
+                {
+                    emitter.count = static_cast<uint32_t>(countVal);
+                }
+
+                ImGui::SliderFloat("Spawn Frequency (sec)", &emitter.frequency, 0.01f, 2.0f, "%.2fs");
+            }
         }
         else
         {
